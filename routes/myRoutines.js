fc08566e6a2e669e1078a02de7c54862d8648150
Cristian <cmioveanu@gmail.com>
2021-03-16 15:52:28 +0000

    display exercises for routines

--- routes/myRoutines.js
+++ routes/myRoutines.js
@@ -9,12 +9,33 @@ const pool = new Pool(dbConfig);
 
 //All routines for the History page
 myRoutines.get('/', (req, res) => {
-    pool.query('SELECT * FROM routines ORDER BY name', (error, results) => {
-        if (error) {
-            throw error;
-        }
-        res.status(200).json(results.rows);
-    })
+
+    //get the names in the routines list, even if routine has no exercises
+    pool.query(`
+        SELECT *
+        FROM routines`,
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
+
+            res.status(200).send(results.rows);
+        });
+});
+
+myRoutines.get('/exercises', (req, res) => {
+    pool.query(`
+        SELECT *
+        FROM exercises_routines
+        JOIN exercises
+        ON exercises_routines.exercise_id = exercises.id`,
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
+
+            res.status(200).send(results.rows);
+        });
 });
 
 /* Routines history
