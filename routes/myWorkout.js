da4376872d0f80138fc8ee34854d6960beec1e5a
Cristian <cmioveanu@gmail.com>
2021-03-21 19:47:14 +0000

    add workout creation route

--- routes/myWorkout.js
+++ routes/myWorkout.js
@@ -21,12 +21,54 @@ myWorkout.get('/:routineID', (req, res) => {
     JOIN exercises
     ON exercises_routines.exercise_id = exercises.id
     WHERE routines.id = ${routineID}
-    ORDER BY exercises.name`, 
-    (error, results) => {
-        if(error) {
-            throw error;
-        }
+    ORDER BY exercises.name`,
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
 
-        res.status(200).send(results.rows);
-    });
+            res.status(200).send(results.rows);
+        });
+});
+
+
+//record new workout
+myWorkout.post('/', (req, res) => {
+    const userID = 1;
+    const totalTime = req.body.totalWorkoutTime;
+    const exercises = req.body.exercises;
+
+    const recordPermission = exercises.some(exercise => exercise.timeUnderLoad > 0);
+
+    if (recordPermission) {
+
+        //create a new workout with the current date
+        pool.query(`
+    INSERT INTO workouts(user_id, date, total_time)
+    VALUES($1, current_timestamp, $2)
+    RETURNING id;
+    `, [userID, totalTime,], (error, results) => {
+            if (error) {
+                throw error;
+            }
+
+            workoutID = results.rows[0].id;
+
+            //record the sets of exercises to the database with the above workout ID
+            exercises.forEach(exercise => {
+                if (exercise.timeUnderLoad > 0) {
+                    pool.query(`
+                INSERT INTO sets(workout_id, exercise_routine_id, time_under_load, negatives)
+                VALUES($1, $2, $3, $4)
+                `, [workoutID, exercise.id, exercise.timeUnderLoad, exercise.negatives], (err, res) => {
+                        if (err) {
+                            throw err;
+                        }
+                    })
+                }
+            })
+
+        });
+
+    }
 });
